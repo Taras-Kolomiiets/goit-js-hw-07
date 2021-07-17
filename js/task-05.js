69bef831846b2fd2058950542e2656934e1feaa6
Taras-Kolomiiets <taraskolomiiets8991@gmail.com>
2021-07-17 14:42:59 +0300

    Finish Task 5

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#name-input');
+const name = document.querySelector('#name-output');
+
+input.addEventListener('input', onInputChange)
+
+function onInputChange(event) {
+    name.textContent = event.currentTarget.value || 'незнакомец';
+};
